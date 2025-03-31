39930b50257ab394e8bf8687a932fb0fa5d367c0
XuanBo <wuxuanbo@foxmail.com>
2025-03-31 10:41:39 +0000

    添加依赖

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,11 @@
 add_rules("mode.debug", "mode.release")
 
-
-add_requires("libpcap", "ldns", "uthash")
+add_requires("libpcap", "ldns", "uthash", "nids")
 
 target("netspy")
     set_kind("binary")
     add_files("src/*.c")
-    add_packages("libpcap", "ldns", "uthash")
+    add_packages("libpcap", "ldns", "uthash", "nids")
 --
 -- If you want to known more usage about xmake, please see https://xmake.io
 --
